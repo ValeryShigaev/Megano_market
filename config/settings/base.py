10bc877f931ee728bac98bee76d2b2ad11570b0b
sergey_klimov <s.klimov@bk.ru>
2022-04-08 16:55:05 +0700

    исправлены ошибки в base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -20,7 +20,7 @@ from environs import Env
 env = Env()
 env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
--- entrypoint.sh
+++ entrypoint.sh
@@ -9,14 +9,14 @@ python manage.py loaddata fixtures/goods_app.product.json
 python manage.py loaddata fixtures/profiles_app.users.json
 python manage.py loaddata fixtures/stores_app._seller.json
 python manage.py loaddata fixtures/discounts_app.cartdiscount.json
-python manage.py loaddata fixtures/goods_app.specificationnames.json
+#python manage.py loaddata fixtures/goods_app.specificationnames.json
 python manage.py loaddata fixtures/profiles_app_social.json
 python manage.py loaddata fixtures/discounts_app.groupdiscount.json
 python manage.py loaddata fixtures/orders_app.order.json
 python manage.py loaddata fixtures/stores_app.sellerproduct.json
 python manage.py loaddata fixtures/discounts_app.productdiscount.json
 python manage.py loaddata fixtures/banners_app.banners.json
-python manage.py loaddata fixtures/orders_app.orderproduct.json
+#python manage.py loaddata fixtures/orders_app.orderproduct.json
 
 python manage.py collectstatic --no-input
 python manage.py compilemessages
